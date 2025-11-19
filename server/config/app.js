7d165bbea2c8361804e1ccb42e37a5b68a4f43d6
RabijanR <rabijan.ragupalan@ontariotechu.net>
2025-11-19 11:03:44 -0500

    fixed navbar issues and got rid of unessesary pages

--- server/config/app.js
+++ server/config/app.js
@@ -8,7 +8,6 @@ var logger = require('morgan');
 let mongoose =  require('mongoose');
 const DB = require('./db');
 var indexRouter = require('../routes/index');
-var usersRouter = require('../routes/users');
 let assignmentsRouter = require('../routes/assignments')
 var app = express();
 
@@ -32,7 +31,6 @@ app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/assignments', assignmentsRouter);
 
 // catch 404 and forward to error handler
